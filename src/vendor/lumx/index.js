e463b8915f3135c6d21b8c3c1b6dfaa8ceb46deb
fancn <fanyj@yikeer.com>
2016-06-01 09:56:56 +0800

    Lumx Support

---
+++ src/vendor/lumx/index.js
@@ -0,0 +1,8 @@
+var css = require('./lumx.css');
+
+require('velocity-animate');
+require("bourbon");
+global.moment = require('moment');
+require("mdi");
+require('./lumx');
+module.exports = "lumx";
